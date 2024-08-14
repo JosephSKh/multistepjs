2f52e375117f939f1c30376451cf478a62e4364d
Joseph Safwat Khella <josephskhella@gmail.com>
2024-08-14 21:04:37 +0300

    Update multistepjs.js

--- multistepjs.js
+++ multistepjs.js
@@ -255,7 +255,7 @@ function disableBtn(_0x59e8ba) {
     });
     // $("[data-form=\"submit-btn\"]").addClass("disabled");
     $("[data-form-ms=\"submit-btn\"]").css({
-      opacity: "0.4",
+      opacity: "1",
       "pointer-events": "auto"
     });
     // $("[data-form-ms=\"submit-btn\"]").addClass("disabled");
