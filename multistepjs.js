8d45ef21aab4686d358e36a89fe715a395770bc9
Joseph Safwat Khella <josephskhella@gmail.com>
2024-08-16 14:26:43 +0300

    Update multistepjs.js

--- multistepjs.js
+++ multistepjs.js
@@ -31,18 +31,21 @@ let popupFormStepStatus = 0;
 $($('#popup-interest .form-step')[0]).show();
 $($('#popup-interest .form-step')[1]).hide();
 $('#popup-interest .form-back-btn').hide();
+// $('#popup-interest .form-back-btn').css('visibility', 'hidden');
 $('#popup-interest .form-submit-btn').hide();
 
 $($('#popup-information .form-step')[0]).show();
 $($('#popup-information .form-step')[1]).hide();
-$('#popup-information .form-back-btn').hide();
+$('#popup-information .form-back-btn').css('visibility', 'hidden');
+// $('#popup-information .form-back-btn').hide();
 $('#popup-information .form-submit-btn').hide();
 
 $('#popup-interest .form-next-btn.popup-btn').click(function(e) {
         $($('#popup-interest .form-step')[1]).show();
         $($('#popup-interest .form-step')[0]).hide();
         
-        $('#popup-interest .form-back-btn').show();
+        // $('#popup-interest .form-back-btn').show();
+        $('#popup-interest .form-back-btn').css('visibility', 'visible');
         $('#popup-interest .form-submit-btn').show();
         $('#popup-interest .form-next-btn.popup-btn').hide();
 });
@@ -50,7 +53,8 @@ $('#popup-interest .form-back-btn.popup-btn').click(function(e) {
         $($('#popup-interest .form-step')[0]).show();
         $($('#popup-interest .form-step')[1]).hide();
         
-        $('#popup-interest .form-back-btn').hide();
+        // $('#popup-interest .form-back-btn').hide();
+        $('#popup-interest .form-back-btn').css('visibility', 'hidden');
         $('#popup-interest .form-submit-btn').hide();
         $('#popup-interest .form-next-btn.popup-btn').show();
 });
@@ -59,7 +63,8 @@ $('#popup-information .form-next-btn.popup-btn').click(function(e) {
         $($('#popup-information .form-step')[1]).show();
         $($('#popup-information .form-step')[0]).hide();
         
-        $('#popup-information .form-back-btn').show();
+        // $('#popup-information .form-back-btn').show();
+        $('#popup-information .form-back-btn').css('visibility', 'visible');
         $('#popup-information .form-submit-btn').show();
         $('#popup-information .form-next-btn.popup-btn').hide();
 });
@@ -67,7 +72,8 @@ $('#popup-information .form-back-btn.popup-btn').click(function(e) {
         $($('#popup-information .form-step')[0]).show();
         $($('#popup-information .form-step')[1]).hide();
         
-        $('#popup-information .form-back-btn').hide();
+        // $('#popup-information .form-back-btn').hide();
+        $('#popup-information .form-back-btn').css('visibility', 'hidden');
         $('#popup-information .form-submit-btn').hide();
         $('#popup-information .form-next-btn.popup-btn').show();
 });
